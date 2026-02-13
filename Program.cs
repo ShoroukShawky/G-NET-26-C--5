874da504af64b62f82aaca9ee1f6d140aee6c471
ShoroukShawky <324243558@sha.edu.eg>
2026-02-13 04:15:32 +0100

    Q3

--- Program.cs
+++ Program.cs
@@ -25,45 +25,72 @@ namespace G_NET_26_C__5
             //}
             #endregion
             #region Q2
-            Console.Write("Enter array size: ");
-           bool sizeValid =  int.TryParse(Console.ReadLine() , out int sizeOfArray );
-            if (!sizeValid || sizeOfArray <= 0)
-            {
-                Console.WriteLine("invalid size!!");
-                return;
-            }
-            int[] array = new int[sizeOfArray];
+            // Console.Write("Enter array size: ");
+            //bool sizeValid =  int.TryParse(Console.ReadLine() , out int sizeOfArray );
+            // if (!sizeValid || sizeOfArray <= 0)
+            // {
+            //     Console.WriteLine("invalid size!!");
+            //     return;
+            // }
+            // int[] array = new int[sizeOfArray];
 
-            int sum = 0 , max = int.MinValue , min = int.MaxValue ;
-            for (int i = 0; i < sizeOfArray; i++)
-            {
-                Console.Write($"Enter element[{i}]:");
-                bool valid = int.TryParse(Console.ReadLine(), out array[i]);
+            // int sum = 0 , max = int.MinValue , min = int.MaxValue ;
+            // for (int i = 0; i < sizeOfArray; i++)
+            // {
+            //     Console.Write($"Enter element[{i}]:");
+            //     bool valid = int.TryParse(Console.ReadLine(), out array[i]);
 
-                if(!valid )
+            //     if(!valid )
+            //     {
+            //         Console.WriteLine("invaild element!!");
+            //         return;
+            //     }
+            //      sum+= array[i];
+            //     if (array[i] < min)
+            //         min = array[i];
+            //     if(array[i] > max)
+            //         max = array[i];
+            // }
+            // double avg = (double)sum / sizeOfArray;
+            // Console.WriteLine();
+            // Console.WriteLine($"Sum = {sum}");
+            // Console.WriteLine($"Average = {avg}");
+            // Console.WriteLine($"Max = {max}");
+            // Console.WriteLine($"Min = {min}");
+            // Console.Write("Reverse = ");
+            // for (int i = sizeOfArray - 1; i >= 0; i--)
+            // {
+            //     Console.Write(array[i]);
+            //     if (i!= 0)
+            //         Console.Write(", ");
+            // }
+            #endregion
+            #region Q3
+            double[,] gradsArray = new double[3, 4];
+            double classSum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"enter grades for student {i+1}: ");
+                double stuSum = 0;
+                  for (int j = 0; j < 4; j++)
                 {
-                    Console.WriteLine("invaild element!!");
-                    return;
+                    bool valid = double.TryParse(Console.ReadLine(), out gradsArray[i, j]);
+                    if (!valid)
+                    {
+                        Console.WriteLine("invalid grade");
+                        return;
+                    }
+
+                    
+                    stuSum += gradsArray[i, j];
                 }
-                 sum+= array[i];
-                if (array[i] < min)
-                    min = array[i];
-                if(array[i] > max)
-                    max = array[i];
+                double stuAvg = stuSum / 4;
+                Console.WriteLine($"student {i+1} average grades = {stuAvg}");
+                classSum += stuSum;
             }
-            double avg = (double)sum / sizeOfArray;
-            Console.WriteLine();
-            Console.WriteLine($"Sum = {sum}");
-            Console.WriteLine($"Average = {avg}");
-            Console.WriteLine($"Max = {max}");
-            Console.WriteLine($"Min = {min}");
-            Console.Write("Reverse = ");
-            for (int i = sizeOfArray - 1; i >= 0; i--)
-            {
-                Console.Write(array[i]);
-                if (i!= 0)
-                    Console.Write(", ");
-            }
+            double classAvg = classSum / 12;
+            Console.WriteLine($"class average grades = {classAvg}");
             #endregion
         }
     }
